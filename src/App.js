ae320ec7b723cc8ba5faabcf9d3a85e33e49011b
Rebecca Zuo <rebeccazuo@Rebeccas-Air.lan>
2024-08-28 15:36:56 -0400

    working color extractor

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
 import React, { useState } from 'react';
-import WebcamCapture from './Webcam';
-// import ColorFinder from  './Colorfinder'
-
+import { ColorExtractor } from 'react-color-extractor'; 
+import WebcamCapture from './Webcam.js'; 
 
 function App() {
   const [imageSrc, setImageSrc] = useState(null);
+  const [colors, setColors] = useState([]);
 
-    return (
-      <div>
-        <h1>Matcha Shade Finder</h1>
-        <WebcamCapture onCapture={setImageSrc} />
-        {/* {imageSrc && <ColorFinder imageSrc={imageSrc} />} */}
+  // Function to render color swatches
+  const renderSwatches = () => {
+    return colors.map((color, id) => (
+      <div
+        key={id}
+        style={{
+          backgroundColor: color,
+          width: 100,
+          height: 100,
+          margin: '5px', // Adding margin for better spacing
+        }}
+      />
+    ));
+  };
+
+  // Function to update the colors state
+  const getColors = (extractedColors) => {
+    setColors([...colors, ...extractedColors]);
+  };
+
+  console.log("image source", imageSrc);
+
+  return (
+    <div>
+      <h1>Matcha Shade Finder</h1>
+      <WebcamCapture onCapture={setImageSrc} />
+      {/* Assuming ColorExtractor expects children to extract colors from */}
+      { imageSrc && (
+      <ColorExtractor getColors={getColors}>
+        {imageSrc && <img src={imageSrc} alt="Captured" />}
+      </ColorExtractor>
+      )}
+      <div
+        style={{
+          marginTop: 20,
+          display: 'flex',
+          flexWrap: 'wrap', // Allows colors to wrap to the next line
+          justifyContent: 'center',
+        }}
+      >
+        {renderSwatches()}
       </div>
-    );
-
+    </div>
+  );
 }
 
 export default App;
